995752870b3a0841857803b086cc17368aa363d5
david <david.schneiter28@mail.com>
2024-04-06 22:47:47 -0300

    Pokemon grid/card

---
+++ src/pokemons/components/PokemonCard.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import React from 'react'
+import { SimplePokemon } from '../interfaces/simple-pokemon';
+
+interface Props {
+    pokemon: SimplePokemon;
+}
+
+export const PokemonCard = ({ pokemon }: Props) => {
+    const { name, id } = pokemon
+    return (
+        <div className="relative flex flex-col justify-center bg-gradient-to-r from-rose-100 to-teal-100">
+            <div className="mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
+                <div className='flex justify-center'>
+
+                    <Image
+                        key={name}
+                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
+                        width={150}
+                        height={150}
+                        alt={`${name}`}
+                    />
+                </div>
+                <div className="p-4">
+                    <small className="text-blue-400 text-xs"># - {id}</small>
+                    <h1 className="text-2xl font-medium text-slate-600 pb-2 capitalize">{name}</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/pokemons/index.ts
@@ -0,0 +1,2 @@
+export type { PokemonResponse } from './interfaces/pokemons'
+export type { SimplePokemon } from './interfaces/simple-pokemon'
