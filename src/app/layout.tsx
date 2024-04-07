518e5a03d21c3bb64b13955de679ce6126d59b25
david <david.schneiter28@mail.com>
2024-04-07 02:16:29 -0300

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={inter.className}>
-        <div className="flex min-w-full justify-center items-center h-20 bg-red-700">
+        <div className="flex min-w-full justify-center items-center h-20 bg-red-700 mb-10">
           <Link href={"/"}>
             Pokedex
           </Link>
